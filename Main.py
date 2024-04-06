3a1f837be67213f5d68c141d95d447840845e854
Jamie Marchant <jamiemarchant095@gmail.com>
2024-04-06 23:58:45 +0100

    Created notbook

--- Main.py
+++ Main.py
@@ -8,19 +8,18 @@ screen.bgcolor("black")
 screen.title("Pong")
 
 right_paddle = Turtle(shape="square")
+right_paddle.setpos(x=350, y=0)
 right_paddle.penup()
 right_paddle.color("white")
 right_paddle.turtlesize(stretch_wid=5, stretch_len=1)
-right_paddle.setpos(x=350, y=0)
-
 
 def up():
-    right_paddle.setheading(up)
+    right_paddle.setheading(90)
 
 
+screen.listen()
 screen.onkey(right_paddle.up, "Up")
 screen.onkey(right_paddle.down, "Down")
-screen.listen()
 
 
 # close game on click
