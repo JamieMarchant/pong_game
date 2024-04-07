ef3b4d26728fdd364317dda52c70ea58701fb92f
Jamie Marchant <jamiemarchant095@gmail.com>
2024-04-07 20:35:33 +0100

    Completed game

--- Main.py
+++ Main.py
@@ -1,26 +1,55 @@
-# Pong!
-from turtle import Screen, Turtle
+from turtle import Screen
+from paddle_classes import Paddle
+from ball import Ball
+import time
+from scoreboard import Scoreboard
 
 # Screen Settings
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)
 
-right_paddle = Turtle(shape="square")
-right_paddle.setpos(x=350, y=0)
-right_paddle.penup()
-right_paddle.color("white")
-right_paddle.turtlesize(stretch_wid=5, stretch_len=1)
+# class creation
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
 
-def up():
-    right_paddle.setheading(90)
-
-
+# key inputs for both paddles
 screen.listen()
-screen.onkey(right_paddle.up, "Up")
-screen.onkey(right_paddle.down, "Down")
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
 
+# main game loop
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
 
+    # Check collision with top and bottom walls
+    if ball.ycor() > 290 or ball.ycor() < -290:
+        ball.bounce_y()
+        
+
+    # Check collison with right and left paddles
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+        
+
+    # check for Collison with right border
+    if ball.xcor() > 390:
+        ball.reset()
+        scoreboard.l_point()
+    
+    # check for collison with left border
+    if ball.xcor() < -390:
+        ball.reset()
+        scoreboard.r_point()
+        
 # close game on click
 screen.exitonclick()
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # pong_game
+Welcome! This is my Pong game made using Turtle graphics. 
+
+To play, simply use the up/down arrows for the right paddle and w/s to move the left paddle. 
+
+The ball will increase in speed each time it touches a paddle, but rests if a point is scored. 
+
+Have fun!
